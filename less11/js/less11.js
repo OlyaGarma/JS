babc914e98f524ac740639aa77066218fff9f000
OlyaGarma <56018207+OlyaGarma@users.noreply.github.com>
2019-12-13 21:04:49 +0300

    Update less11.js

--- less11/js/less11.js
+++ less11/js/less11.js
@@ -167,3 +167,10 @@ validator.init({
   successHandler: successHandler,
   errorHandler: errorHandler
 });
+
+// Cамолет Москва - Нью-йорк. Время в полете 10 часов. Когда (дата и время) самолет прилетит в Нью-йорк (по местному времени), если из Москвы он вылетает в 22:15 28 декабря 2019 года
+
+let moscowTime = moment("2019-12-28 22:15").tz("Europe/Moscow");
+let nyTime = moscowTime.add(10, 'hours');
+let nyTime1 = moscowTime.clone().tz("America/New_York");
+console.log('Прилет в Нью-Йорк: ' + nyTime1.format("YYYY MMMM DD HH:mm"));
